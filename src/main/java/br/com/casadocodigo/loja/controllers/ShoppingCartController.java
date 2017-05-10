910e223ee07528197aac0872aa249c4390f20026
williammian <william_mian@yahoo.com.br>
2017-05-10 17:09:38 -0300

    carrinho

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ShoppingCartController.java
@@ -0,0 +1,61 @@
+package br.com.casadocodigo.loja.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProductDAO;
+import br.com.casadocodigo.loja.models.BookType;
+import br.com.casadocodigo.loja.models.Product;
+import br.com.casadocodigo.loja.models.ShoppingCart;
+import br.com.casadocodigo.loja.models.ShoppingItem;
+
+@Controller
+@RequestMapping("/shopping")
+@Scope(value=WebApplicationContext.SCOPE_REQUEST)
+public class ShoppingCartController {
+	
+	@Autowired
+	private ProductDAO productDAO;
+	@Autowired
+	private ShoppingCart shoppingCart;
+	
+	@InitBinder
+    protected void initBinder(HttpSession session) {
+		session.setAttribute("shoppingCart", shoppingCart);
+    }
+
+	@RequestMapping(method=RequestMethod.POST)	
+	public ModelAndView add(Integer productId,@RequestParam BookType bookType){
+		System.out.println(bookType);
+		ShoppingItem item = createItem(productId, bookType);
+		shoppingCart.add(item);
+		return new ModelAndView("redirect:/produtos");
+	}
+
+	private ShoppingItem createItem(Integer productId, BookType bookType) {
+		Product product = productDAO.get(productId);
+		ShoppingItem item = new ShoppingItem(product,bookType);
+		return item;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String items(){
+		return "shoppingCart/items";
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/${productId}")
+	public String remove(@PathVariable("productId") Integer productId,BookType bookType){
+		shoppingCart.remove(createItem(productId, bookType));
+		return "redirect:/shopping";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/shoppingCart/items.jsp
@@ -0,0 +1,42 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<html>
+<head>
+</head>
+<body>
+<h2>Seu carrinho de compras</h2>
+<table>
+	<thead>
+    	<tr>
+	        <th>Item</th>
+	        <th>Preço</th>
+	        <th>Quantidade</th>
+	        <th>Total</th>
+	        <th>Remover</th>
+        </tr>
+	</thead>
+	<tbody>
+        <c:forEach items="${shoppingCart.list}" var="item">
+        <tr>
+          <td>${item.product.title} - ${item.bookType}</td>
+          <td>${item.price}</td>
+          <td>${shoppingCart.getQuantity(item)}</td>
+          <td>${shoppingCart.getTotal(item)}</td>
+          <td>
+          	<form method="post" action="${spring:mvcUrl('SCC#remove').arg(0,item.product.id).arg(1,item.bookType).build()}">
+          		<input type="submit" value="Remover" />
+          	</form>
+          </td>
+        </tr>
+        </c:forEach>
+        
+      </tbody>
+      <tfoot>
+        <tr>
+          <td><input type="submit" class="checkout" name="checkout" value="Finalizar compra " id="checkout"/></td>
+          <td>Total: ${shoppingCart.total}</td><td></td>
+        </tr>
+      </tfoot>
+</table>
+</body>
+</html>
