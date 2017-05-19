240daf21d170a7e48e70503f1acf6ad3e48cb828
williammian <william_mian@yahoo.com.br>
2017-05-19 16:16:53 -0300

    teste

--- src/test/java/br/com/casadocodigo/loja/builders/Testando.java
+++ src/test/java/br/com/casadocodigo/loja/builders/Testando.java
@@ -5,6 +5,7 @@ import br.com.casadocodigo.loja.daos.ProductDAOTest;
 
 public class Testando {
 	public static void main(String[] args) {
+		System.out.println("Teste");
 		org.junit.runner.JUnitCore.runClasses(ProductDAOTest.class);
 	}
 }
