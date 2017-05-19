33f9b78fd83a41f7736fd6e914350c641596e9fe
williammian <william_mian@yahoo.com.br>
2017-05-19 15:52:36 -0300

    test

---
+++ src/test/java/br/com/casadocodigo/builders/Testando.java
@@ -0,0 +1,10 @@
+package br.com.casadocodigo.builders;
+
+import br.com.casadocodigo.loja.daos.ProductDAOTest;
+
+
+public class Testando {
+	public static void main(String[] args) {
+		org.junit.runner.JUnitCore.runClasses(ProductDAOTest.class);
+	}
+}
